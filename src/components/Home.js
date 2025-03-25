27534796c1573e1be080fbc4756c0e8371c8c466
nilakshiayoma <nilakshiayoma.12@cse.mrt.ac.lk>
2025-03-25 21:47:03 +1100

    set up project

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to My React App</h1>
+      <p>This is the home page of our application.</p>
+    </div>
+  );
+}
+
+export default Home; 
